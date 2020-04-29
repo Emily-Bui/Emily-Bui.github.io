23754ad752416ff16af2318913a9a2db7c8f284f
Emily Bui <emily.bui@berkeley.edu>
2020-04-29 03:33:26 -0500

    popup

--- assets/styles/script.js
+++ assets/styles/script.js
@@ -1,1 +1,12 @@
+function unhideLightbox(lightboxID) {
 
+	document.getElementById(lightboxID).classList.remove('hidden');
+}
+
+
+function unhideLightbox1() {
+	
+	unhideLightbox("popup1");
+}
+
+document.getElementById("case").onclick = unhideLightbox1
